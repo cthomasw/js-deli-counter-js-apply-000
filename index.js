ae403df140b7aa58df9c4d813f27b39c552e59c2
Claire Wilbert <claire.wilbert@gmail.com>
2019-05-01 03:05:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function takeANumber(theLine, customerName) {
+  let placeInLine = theLine.push(customerName) + 1;
+  return `Welcome, ${customerName}. You are number ${placeInLine} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    let firstInLine = katzDeliLine.shift();
+    return `Currently serving ${firstInLine}.`
+  }
+}
+
+function currentLine(coolCatsInLine) {
+  if (coolCatsInLine.length == 0) {
+    return 'The line is currently empty.';
+  } else {
+    for (cat of coolCatsInLine) {
+      
+    }
+    return `The line is currently: `
+  }
+}
