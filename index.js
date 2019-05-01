77e2346b21c2c193c480cdbef59be2cb6e2b3f00
Claire Wilbert <claire.wilbert@gmail.com>
2019-05-01 03:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(coolCatsInLine) {
   if (coolCatsInLine.length == 0) {
     return 'The line is currently empty.';
   } else {
-    let catsStillInLine = 'The line is currently:';
+    let catsStillInLine = 'The line is currently: ';
     let cat = '';
     for (cat of coolCatsInLine) {
       let placeInLine = coolCatsInLine.indexOf(cat) + 1;
